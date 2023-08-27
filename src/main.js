48d2b51fd850a00ec36d8670759d8b84e5bf8121
Timea Szucs <szucstimea02@gmail.com>
2023-08-27 19:53:28 +0200

    +Bootstrap, +FontAwesome, +AppHeader.vue

--- src/main.js
+++ src/main.js
@@ -4,8 +4,34 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+//import bootstrap
+import "bootstrap/dist/css/bootstrap.css"
+import "bootstrap/dist/js/bootstrap.js"
+
+//Import FontAwesome
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import {
+    faCartShopping,
+    faEnvelope,
+    faMagnifyingGlass,
+    faPhone,
+    faUser
+} from '@fortawesome/free-solid-svg-icons'
+
+library.add(
+    faCartShopping,
+    faMagnifyingGlass,
+    faEnvelope,
+    faPhone,
+    faUser
+)
+
 const app = createApp(App)
 
-app.use(router)
+app
+.use(router)
 
-app.mount('#app')
+app
+.component('font-awesome-icon', FontAwesomeIcon)
+.mount('#app')
